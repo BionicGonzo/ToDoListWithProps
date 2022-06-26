f080fb8ad4fa96ccf03f9f5014457866f159712c
Gonzo <gonzalo.araosolguin@gmail.com>
2022-06-26 19:34:57 -0400

    To Do List with Props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="p-3">
+    <div class="col-md-6 mx-auto">
+      <div class="card p-2">
+        <div class="card-title">
+          <h2>Ingresar Tarea</h2>
+        </div>
+        <div class="card-body">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Ingresar Tarea</span>
+            <input v-model="tarea" @keyup.enter="addTarea" type="text" class="form-control" aria-label="Tarea" aria-describedby="basic-addon1">
+          <button @click="addTarea" class="btn btn-outline-dark">Agregar</button>
+          </div>
+        </div>
+        <Tareas mensaje="Lista de Tareas" :listaTareas="listaTareas" @remove="removeTareaEnviada"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tareas from "@/components/tareas";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tareas
+  },
+  data(){
+    return {
+      tarea: '',
+      listaTareas: [],
+    }
+  },
+  methods: {
+    addTarea(){
+      const tarea = this.tarea
+      if (tarea !== '') {
+        this.listaTareas.push(tarea)
+      }
+      console.log(this.listaTareas)
+      this.tarea = ''
+    },
+    removeTareaEnviada(item){
+      console.log(item)
+      for(let i in this.listaTareas) {
+        if (this.listaTareas[i] === item) {
+          var n = parseInt(i)
+        }
+      }
+      this.listaTareas.splice(n, 1)
+    }
   }
 }
 </script>
@@ -23,6 +61,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/tareas.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h2>{{mensaje}}</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Tarea</th>
+                <th scope="col"><i class="fa-solid fa-trash"></i></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in listaTareas" :key="index">
+                <th>{{index + 1}}</th>
+                <td>{{item}}</td>
+                <td>
+                    <button @click="removeTarea(item)" class="btn btn-outline-danger btn-sm" type="submit">
+                        <i class="fa-solid fa-trash text-danger"></i>
+                    </button>
+                </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    name: "Tareas",
+    props: {
+        mensaje: String,
+        listaTareas: Array
+    },
+    methods: {
+        removeTarea(item) {
+            this.$emit('remove', item)
+        }
+    }
+}
+</script>
+<style scope>
+</style>
